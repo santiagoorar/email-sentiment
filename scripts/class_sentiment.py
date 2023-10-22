1f2480beee829c0ed72c0febd1832f65c45b6bb2
santiagoormando <sormando@correo.um.edu.uy>
2023-10-22 09:31:02 +0200

    Class sentiment done

---
+++ scripts/class_sentiment.py
@@ -0,0 +1,12 @@
+class SentimentDataset:
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        item['labels'] = self.labels[idx]
+        return item
+
+    def __len__(self):
+        return len(self.labels)
